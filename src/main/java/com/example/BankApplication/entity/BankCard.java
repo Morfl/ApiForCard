f399754765535b0afd4541eea71c76917c4ff7bc
Makss <makssimm020@gmail.com>
2025-04-21 04:22:22 +0700

    test

---
+++ src/main/java/com/example/BankApplication/entity/BankCard.java
@@ -0,0 +1,20 @@
+package com.example.BankApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnTransformer;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+    @Entity
+    @Table(name = "bank_cards")
+    @Getter
+    @Setter
+    public class BankCard {
+
+
+}
+
+
